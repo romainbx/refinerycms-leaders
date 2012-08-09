858c3c9f3e4d7d2c63bf3aecd51d821a306ebd3d
Romain Binaux <romain@secondbureau.com>
2012-08-09 18:05:05 +0800

    DEleted files

--- db/migrate/3_create_leaders_groups_persons_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateLeadersGroupsPersonsJoinTable < ActiveRecord::Migration
-  def change
-    create_table :refinery_leaders_groups_persons, :id => false do |t|
-      t.integer :group_id
-      t.integer :person_id
-    end
-  end
-end
